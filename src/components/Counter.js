464e2c6248ab2d228e86806b770d4a77f0e3b9e2
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-07-06 21:13:29 +0530

    feat: subscribed Counter component to redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,37 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // useDispatch returns a function which can be used to dispatch actions which
+  // make their way to reducer function which then mutates the central storage
+  const dispatch = useDispatch();
+
+  // useSelector helps us to get a piece or slice of data from central-data store
+  // this piece of data we get is determined by the function we pass as an argument
+  // to useSelector and useSelector also makes sure the component we are using
+  // useSelector in get's subscribed to the central-store
+
+  // also if this component unmounts react-redux makes sure to unsubscribe from store
+  const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
